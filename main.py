9aa850503c988121ca9e6e6b300a8955bbee44b7
schlueterr <schlueter.braden@gmail.com>
2022-12-01 15:48:22 -0600

    supports multiple users, needs updated valid credentials

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 VALID_USERNAME = "Richard"
 VALID_PASSWORD = "Testing123"
 
+VALID_CREDENTIALS = []
 
+# Function checks if the username and password are correct given user input
 def correctCredentialChecker(userName, password):
+    credentials_stored = None;
+    for cred in VALID_CREDENTIALS:
+        if cred['username'] == userName:
+            credentials_stored = cred
+            break
+    
+    if credentials_stored is None:
+        return False
+    
+    return credentials_stored['password'] == password
+
+if __name__ == "__main__":
     userName = input("Enter your username: ")
     password = input("Enter your password: ")
-
+    
     if correctCredentialChecker(userName, password):
         print("Welcome to the system!")
     else:
         print("Invalid username or password. Please try again.")
-
-
