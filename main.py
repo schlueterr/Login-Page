144bfe3cb16f1d953916bd526c4f35fc35b68541
schlueterr <schlueter.braden@gmail.com>
2022-11-30 20:30:30 -0600

    setting up hardcoded verification

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("testing ")
+VALID_USERNAME = "Richard"
+VALID_PASSWORD = "Testing123"
+
+
+def correctCredentialChecker(userName, password):
+    userName = input("Enter your username: ")
+    password = input("Enter your password: ")
+
+    if correctCredentialChecker(userName, password):
+        print("Welcome to the system!")
+    else:
+        print("Invalid username or password. Please try again.")
+
+
