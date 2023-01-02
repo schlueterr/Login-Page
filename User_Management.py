a92f24f86ec2d2d00ea8a382b11b3fa6ea9fc518
schlueterr <schlueter.braden@gmail.com>
2023-01-02 15:10:12 -0600

    User_Management.py is working and is able to add users to the database Database is working as of 1/2/2023 as well as all other files

--- User_Management.py
+++ User_Management.py
@@ -14,4 +14,36 @@ def username_available(userName):
                          """ , (userName,)).fetchone()
     
     return result is None
+
+def add_user(userName, password):
+    conn = sqlite3.connect(DATABASE_NAME)
+    cur = conn.cursor()
+    
+    hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
+    
+    cur.execute("""
+              INSERT INTO 
+                users (Username, Password)
+               VALUES
+                (?, ?)
+                """, (userName, hashed_password))
+    conn.commit()
+    # Not sure if this sqlite table setup is correct
+    # Table is setup correctly and the code works as intended
+    
+if __name__ == "__main__":
+    print ("Let's set up your account as a new user.")
+    
+    while True:
+        userName = input("Enter your username: ")
+        if username_available(userName):
+            print ("Username is available.")
+            break
+        else:
+            print("Username is not available. Please try again.")
+            
+    password = input("Enter your password: ")
+    
+    add_user(userName, password)
+    print("Added user " + userName + " to the database.")
                     
